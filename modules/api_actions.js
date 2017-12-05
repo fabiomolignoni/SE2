3078f6d0df38e6930ad8b42c14e0ece0077c4b33
Giacomo Callegari <giacomo.callegari@studenti.unitn.it>
2017-12-05 18:26:30 +0100

    add ability to search ads with bot

--- modules/api_actions.js
+++ modules/api_actions.js
@@ -8,6 +8,13 @@ const config = require('../config');
 //Richieste HTTP
 const axios = require('axios');
 
+//Gestione bot di Telegram
+const Telegraf = require('telegraf');
+
+//Controllo del flusso
+const TelegrafFlow = require('telegraf-flow');
+const { WizardScene } = TelegrafFlow;
+
 //Markup del bot
 const { Markup } = require('telegraf');
 
@@ -17,153 +24,174 @@ const { Markup } = require('telegraf');
 // FUNZIONI //
 //////////////
 
-//Cerca 3 annunci secondo i parametri richiesti
+
+//Recupera gli annunci tramite API
+function getAds(bot, ctx, url) {
+    //RICHIESTA ANNUNCI
+    axios.get(url)
+    .then(function (response) {
+        //console.log(response);
+        var ads = response.data.ads;
+        
+        if(!ads) {
+            return ctx.reply('Errore.');
+        }
+
+        //Itero sugli annunci trovati
+        for (var i = 0; i < ads.length; i++) {
+            var ad = ads[i];
+
+            console.log('Trovato \'' + ad.title + '\'');
+
+            //Ricavo le informazioni sull'autore
+            var author = axios.get(ad.author)
+            .then (function (response) {
+                var author = response.data;
+
+                ctx.replyWithMarkdown('Annuncio ' + i + '\n' +
+                          '*' + ad.title + '*\n' +
+                          'Descrizione: ' + ad.desc + '\n' +
+                          'Prezzo: ' + ad.price + '\n' +
+                          'Categoria: ' + ad.category + '\n' +
+                          'Data: ' + ad.date + '\n' +
+                          'Autore: ' + author.name + ' ' + author.surname + '\n'
+                      );
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+        }
+    })/*
+    .then(function () {
+        ctx.reply('/continua per vedere altri annunci');
+    })*/
+    .catch(function (err) {
+        console.log(err);
+    });
+}
+
+//Gestisce la ricerca degli annunci
 function searchAds(bot, ctx) {
     
+    console.log('*RICERCA ANNUNCI*');
     
+    //Parametri della richiesta GET
+    var site = config.site;
+    var q = '';
+    var title = '';
+    var category = '';
+    var lessThan = '';
+    var limit = 3;
+    var offset = '';
+    var fromLast = '';
+    var user = '';
     
-    //Chiedi testo
-        //'Scrivi cosa vuoi cercare.'
-            //TESTO
-    //Chiedi categoria
-        //'Filtra per categoria.'
-            //PULSANTI (all, libri, appunti, stage/lavoro, ripetizioni, eventi)
-    //Chiedi prezzo
-        //'Filtra per prezzo.'
-            //PULSANTI (tutto, scegli massimo, gratis)
-    //Chiedi autore
-        //'Filtra per autore.'
-            //PULSANTI (tutto, scegli autore)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    console.log('Ricerca annunci');
+    //QUERY
+    const queryScene = new WizardScene('query',
+                                   (ctx) => {
+        console.log('query 1');
+        ctx.reply('Cosa stai cercando?');
+        ctx.flow.wizard.next();
+    },
+                                   (ctx) => {
+        console.log('query 2');
+        if (!ctx.message) {
+            return ctx.reply('Non ho capito.');
+        }
+        q = ctx.message.text;
+        console.log('Query: ' + q);
         
-    //Selezione categoria
-    const text = 'Che cosa stai cercando?';
-    ctx.reply(text, 
-                    Markup.inlineKeyboard([
-                        Markup.callbackButton('Tutto', 'all'),
-                        Markup.callbackButton('Libri', 'libri'),
-                        Markup.callbackButton('Appunti', 'appunti'),
-                        Markup.callbackButton('Stage/Lavoro', 'stage/lavoro'),
-                        Markup.callbackButton('Ripetizioni', 'ripetizioni'),
-                        Markup.callbackButton('Eventi', 'eventi')
-                        ]).extra()
-                    );
+        ctx.flow.enter('category');
+    }
+                                      );
     
-    
-    //Ricerca per categoria
-    bot.on('callback_query', ctx => {
-   
-        //Salvo il messaggio ricevuto e il suo mittente
-        const category = ctx.update.callback_query.data;
-        const userId = ctx.update.callback_query.from.id;
-        
+    //CATEGORIA
+    const categoryScene = new WizardScene('category',
+                                          (ctx) => {
+        console.log('category 1');        
+        ctx.reply('Filtra per categoria.', Markup.inlineKeyboard([
+            Markup.callbackButton('Tutto', 'tutto'),
+            Markup.callbackButton('Libri', 'libri'),
+            Markup.callbackButton('Appunti', 'appunti'),
+            Markup.callbackButton('Stage/Lavoro', 'stage/lavoro'),
+            Markup.callbackButton('Ripetizioni', 'ripetizioni'),
+            Markup.callbackButton('Eventi', 'eventi')
+        ]).extra()
+                 );
+        ctx.flow.wizard.next();
+    },
+                                          (ctx) => {
+        console.log('category 2');        
+        category = ctx.update.callback_query.data;
         console.log('Categoria: ' + category);
-
-        ctx.answerCbQuery('Attendi...');
-        ctx.reply('Hai scelto la categoria ' + category);    
-        ctx.reply('Inserisci il prezzo massimo (0 per articoli gratis).');
+        ctx.reply('Hai scelto la categoria ' + category);
         
-        bot.on('text', ctx => {
-            
-            const text = ctx.message.text;
-            
-            try {
-                maxPrice = parseFloat(text);
-            } catch (err) {
-                console.log(err);
-            }
-            
-            if (isNaN(maxPrice)) {
-                ctx.reply('Inserisci un valore numerico.');
-            } else {
-                console.log('Prezzo massimo: ' + maxPrice);
-                
-                /**/
-                //Parametri della richiesta GET
-                var site = config.site;
-                var q = '';
-                var title = '';
-                var category = ((category == 'all') ? '' : category);
-                var lessThan = maxPrice;
-                var limit = 3;
-                var offset = '';
-                var fromLast = true;
-                var user = '';
-                
-                //RICHIESTA ANNUNCI
-                axios.get(`${site}/ads/?q=${q}&title=${title}&category=${category}&lessThan=${lessThan}&limit=${limit}&offset=${offset}&fromLast=${fromLast}&user=${user}`)
-                .then(function (response) {
-                    var ads = response.data.ads;
-                    
-                    //Itero sugli annunci trovati
-                    for (var i = 0; i < ads.length; i++) {
-                        var ad = ads[i];
-                        
-                        console.log('Annuncio ' + ads.title + ' trovato');
-                        
-                        //Ricavo le informazioni sull'autore
-                        var author = axios.get(ad.author)
-                        .then (function (response) {
-                            var author = response.data;
-                            
-                            ctx.reply('Annuncio ' + i + '\n' +
-                                      'Titolo: ' + ad.title + '\n' +
-                                      'Descrizione: ' + ad.desc + '\n' +
-                                      'Prezzo: ' + ad.price + '\n' +
-                                      'Categoria: ' + ad.category + '\n' +
-                                      'Data: ' + ad.date + '\n' +
-                                      'Autore: ' + author.name + ' ' + author.surname + '\n'
-                                  );
-                        })
-                        .catch(function (err) {
-                            console.log(err);
-                        });
-                        
-                    }
-                })
-                /*.then(function () {
-                    ctx.reply('/continua per vedere altri annunci');
-                })*/
-                .catch(function (err) {
-                    console.log(err);
-                });
-                /**/
-            }
-            
-        });
+        ctx.flow.enter('maxPrice');
+        }
+                                         );
     
+    //PREZZO MASSIMO
+    const maxPriceScene = new WizardScene('maxPrice',
+                                          (ctx) => {
+        console.log('maxPrice 1');        
+        ctx.reply('Filtra per prezzo.', Markup.inlineKeyboard([
+            Markup.callbackButton('Tutto', 'tutto'),
+            Markup.callbackButton('Scegli massimo', 'max'),
+            Markup.callbackButton('Gratis', 'gratis')
+        ]).extra()
+                 );
+        ctx.flow.wizard.next();
+    },
+                                          (ctx) => {
+        console.log('maxPrice 2');
+        const maxPriceChoice = ctx.update.callback_query.data;
+        console.log('Scelta: ' + maxPriceChoice);
         
+        if (maxPriceChoice == 'max') {
+            ctx.reply('Scegli il prezzo massimo.');
+            ctx.flow.wizard.next();
+        } else {
+            ctx.flow.enter('search');
+        }
+    },
+                                          (ctx) => {
+        console.log('maxPrice 3');
+        lessThan = parseFloat(ctx.message.text);
         
+        if (isNaN(lessThan)) {
+            return ctx.reply('Inserisci un valore numerico.');
+        } else if (lessThan < 0) {
+            return ctx.reply('Inserisci un valore positivo.');
+        }
         
-    
+        console.log('Prezzo massimo: ' + lessThan);
+        ctx.flow.enter('search');
     });
+    
+    //ANNUNCI
+    const searchScene = new WizardScene('search',
+                                          (ctx) => {
+        console.log('search 1');
+        const url = `${site}/ads/?q=${q}&title=${title}&category=${category}&lessThan=${lessThan}&limit=${limit}&offset=${offset}&fromLast=${fromLast}&user=${user}`;
+        getAds(bot, ctx, url);
+        ctx.flow.leave();
+    });
+    
+    const flow = new TelegrafFlow([queryScene, categoryScene, maxPriceScene, searchScene], { defaultScene: 'query' });
+    bot.use(Telegraf.session());
+    bot.use(flow.middleware());
+    
 }
 
 //Cerca l'utente richiesto
 function searchUsers(bot, ctx, user) {
-    console.log('Ricerca utenti');
+    console.log('*RICERCA UTENTI*');
     
+    console.log('WIP');
+    ctx.reply('WIP');
+    
+    /*
     var site = config.site;
     //var user = ctx.user;
     var user = '5a16ffc34adab400040f1d39'; //Esempio per debug
@@ -186,6 +214,7 @@ function searchUsers(bot, ctx, user) {
     .catch(function (err) {
         console.log(err);
     });
+    */
 }
 
 module.exports.searchAds = searchAds;
--- modules/telegrambot.js
+++ modules/telegrambot.js
@@ -12,6 +12,15 @@ const axios = require('axios');
 const Telegraf = require('telegraf');
 const bot = new Telegraf('492970626:AAEa0HmNUlV82sPYN1LY5QRSXjay2xjVcTE');
 
+/*
+//Controllo del flusso
+const TelegrafFlow = require('telegraf-flow');
+const { miabScene } = TelegrafFlow;
+const flow = new TelegrafFlow([miab], { defaultScene: 'miab' });
+bot.use(Telegraf.session());
+bot.use(flow.middleware());
+*/
+
 //Markup bot
 const { Markup } = require('telegraf');
 
@@ -26,20 +35,24 @@ function launchbot(){
     console.log('Bot avviato');
     
     //Visualizza i comandi disponibili
-    bot.command('/help', ({ reply }) => reply('/help - Visualizza i comandi disponibili\n' +
+    bot.command('help', ({ reply }) => reply('/help - Visualizza i comandi disponibili\n' +
                                               '/cerca - Cerca un annuncio\n' +
                                               '/continua - Cerca altri annunci simili\n' +
                                               '/contatta - Visualizza come contattare il venditore\n' +
                                               '/sito - Apri il sito web di MessageInABOT'));
     
     //Cerca un annuncio
-    bot.command('/cerca', ctx => api.searchAds(bot, ctx));
+    bot.command('cerca', ctx => api.searchAds(bot, ctx));
     
     //Cerca un autore
-    bot.command('/contatta', ctx => api.searchUsers(bot, ctx));
+    bot.command('contatta', ctx => api.searchUsers(bot, ctx));
     
     //Apre il sito web di MessageInABOT
-    bot.command('/sito', ({ reply }) => reply('Clicca sul link per accedere al sito!\n https://fabiomolignoni.github.io/SE2/'));
+    bot.command('sito', ({ reply }) => reply('Clicca sul link per accedere al sito!\n https://fabiomolignoni.github.io/SE2/'));
+    
+    
+    //bot.use(Telegraf.session());
+    //bot.use(api.flow.middleware());
         
     //Mette il bot in ascolto
     bot.startPolling();
