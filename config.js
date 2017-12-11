fe453328caeafb0f82fc39d206d8cca02b1bd72b
fabiomolignoni <fabio.molignoni@studenti.unitn.it>
2017-12-11 18:23:42 +0100

    Update website url

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     'privatekey': 'Ke79EXBVUEyAzgpH',
     'database': 'mongodb://prova:prova@ds149535.mlab.com:49535/provaautenticazione', //DB di prova
     'site': 'https://messageinabot.herokuapp.com',
-    'homepage': 'https://fabiomolignoni.github.io/SE2/'
+    'homepage': 'https://fabiomolignoni.github.io/SE2/index.html'
 };
